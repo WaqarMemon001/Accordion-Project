7767f2143967e9047aa7277d261ea07a13cbdc02
Waqarmemon001 <itx.vicky.o@gmail.com>
2023-07-19 18:53:50 +0500

    first commit for uploading on git

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import data from "./data";
+
+import "./App.css";
+import SingleQuestion from "./Question";
 
 function App() {
+  const [questions, setQuestions] = useState(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <h3>Frequently Asked Questions</h3>
+        <secton className="info">
+          {questions.map((question) => {
+            return (
+            <SingleQuestion key={question.key} {...question}></SingleQuestion>
+            );
+          })}
+        </secton>
+      </section>
+    </main>
   );
 }
 
